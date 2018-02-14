ea09ca434778ea86307ce152a0fe88720f662cdb
Nick Katarow <nick@turnstylestudio.com>
2018-02-14 14:16:58 -0800

    restructuring styleguide and build directories

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,18 +2,44 @@ module.exports = {
   require: [
     './src/_settings/base.css',
   ],
-  ignore: [
-    '**/atoms/hamburger/*.js',
-    '**/atoms/picture/*.js',
-    '**/atoms/forms/*.js',
-    '**/molecules/_layouts/HalfHalf/*.js',
-    '**/molecules/PrimaryNav/*.js',
-    '**/molecules/UtilityNav/*.js',
-    '**/organisms/Accordion/**/*.js',
-    '**/organisms/Header/*.js',
-    '**/organisms/Footer/*.js',
+  sections: [
+    {
+      name: 'Atoms',
+      components: () => [
+        './src/components/atoms/button/index.js',
+        './src/components/atoms/title/index.js',
+      ],
+      sections: [
+        {
+          name: 'Forms',
+          components: () => [
+            './src/components/atoms/forms/input/index.js',
+            './src/components/atoms/forms/select/index.js',
+            './src/components/atoms/forms/textarea/index.js',
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Molecules',
+      components: () => [
+        './src/components/molecules/Blockquote/index.js',
+        './src/components/molecules/CTA/index.js',
+        './src/components/molecules/Hero/index.js',
+        './src/components/molecules/Introduction/index.js',
+        './src/components/molecules/MediaBlock/index.js',
+        './src/components/molecules/ProductIntro/index.js',
+      ],
+    },
+    {
+      name: 'Organisms',
+      components: () => [
+        './src/components/organisms/Accordion/index.js',
+        './src/components/organisms/MediaList/index.js',
+        './src/components/organisms/Video/index.js',
+      ],
+    },
   ],
   assetsDir: './src/',
-  styleguideDir: './static/',
-  components: './src/components/**/**/*.js',
+  // styleguideDir: './static/',
 };
