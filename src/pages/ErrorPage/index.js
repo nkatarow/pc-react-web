4b1c2fbf6979279ee2e8930d225a85c4e936868d
Nick Katarow <nick@turnstylestudio.com>
2018-02-23 11:29:07 -0800

    error page build

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Atoms
 import Title from '../../components/atoms/title/';
+import Button from '../../components/atoms/button/';
+import Picture from '../../components/atoms/picture/';
+
+// Molecules
+import HalfHalf from '../../components/molecules/_layouts/HalfHalf/';
+
+import image from './_img/not-found.svg';
+import './style.css';
 
 const ErrorPage = () => (
-  <Title tag="h1" theme="hero-title">ERROR PAGE</Title>
+  <section className="error-page">
+    <HalfHalf>
+      <div>
+        <Title tag="h2" theme="hero-title">Oops, something went wrong.</Title>
+        <p className="medium gray">We couldn’t find the page you’re looking for. Please try again later or review other pages on our site for the information you’re seeking.</p>
+        <Link to="/">
+          <Button>DATA SOLUTIONS HOME</Button>
+        </Link>
+      </div>
+      <div>
+        <Picture src={image} />
+      </div>
+    </HalfHalf>
+  </section>
 );
 
 export default ErrorPage;
